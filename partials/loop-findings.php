5910abcaef4344996fe45127bf4a6b169b7809c2
alastair38 <alastair@alastaircox.com>
2015-08-18 13:49:10 +0100

    Version 1.1.2 - fix for problematic output of resource_authors in single-events.php

--- partials/loop-findings.php
+++ partials/loop-findings.php
@@ -28,44 +28,26 @@ $postid = get_the_ID();
                             <h4>Presented Findings</h4>
                             <?php foreach( $user_findings as $user_finding): ?>
 
-                            <?php $permalink = get_permalink($user_finding->ID);?>
+                            <?php $permalink = get_permalink($user_finding->ID);
+                            $authors = get_field('resource_author', $user_finding->ID );
+                            ?>
                             <?php $content = get_post_field('post_content', $user_finding->ID);?>
                                 <h5><a href="<?php echo $permalink; ?>" title="Go to <?php echo get_the_title( $user_finding->ID ); ?>"><?php echo get_the_title( $user_finding->ID ); ?></a></h5>
-
-
+                            <ul class="findings-authors">
+                            <li>Authors: </li>
+                            <?php foreach( $authors as $author):
+                            echo '<li><a href="' . get_permalink( $author->ID ) . '">' . get_the_title( $author->ID ).'</a></li>' . ', ';?>
+                            <?php endforeach; ?>
+                            </ul>
                         <?php endforeach; ?>
 
                         <?php endif; ?>
-
-                        <ul class="findings-authors">
 
 <!-- Get the child categories of Finding that the post is assigned to eg: Presentations, Policy Output, Publications -->
 
 
 <!-- Get the authors assigned to the finding -->
 
-                            <?php $authors = get_field('resource_author', $user_finding->ID ); ?>
-
-							<li>Authors: </li>
-
-
-							<?php
-                            $authorstr = '';
-                            foreach( $authors as $author):
-                            $authorstr .= '<li><a href="' . get_permalink( $author->ID ) . '">' . get_the_title( $author->ID ).'</a></li>' . ', ';
-
-
-                           endforeach;
-      ?>
-
-      <?php echo rtrim($authorstr, ', '); ?>
-
-
-
-
-						<?php // endif; ?>
-
-  </ul>
 
   <hr>
                         </div>
